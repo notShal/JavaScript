7557996fd965ea51abee5005a268a2aa85c61e06
notShal <shaldanillo@gmail.com>
2023-09-16 16:20:51 -0300

    ex11

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 20.5
+console.log(`A velociadade do seu carro é de ${vel}km/h`)
+if (vel > 60) {  // condição simples
+    console.log('Você ultrapassou a velocidade permitada. MULTADO!')
+}
+console.log('Dirija sempre usando o cinto de segurança!')
